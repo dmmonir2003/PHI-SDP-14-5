c4e2a4800239cf78e121208cdb9d644407e39164
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-13 00:44:25 +0600

    setup practice-14.5

---
+++ first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from first_app.forms import TestForm
+
+# Create your views here.
+
+def home(request):
+
+    forms=TestForm()
+
+
+
+    return render(request,'home.html',{'forms':forms})
