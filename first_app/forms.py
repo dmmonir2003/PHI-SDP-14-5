03a2eb17a8302a3df3fc390042bd9d982bb6bc2a
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-13 01:01:29 +0600

    DateField() with SelectDateWidget widget1~and BooleanField()and DateField()

--- first_app/forms.py
+++ first_app/forms.py
@@ -2,3 +2,10 @@ from django import forms
 
 class TestForm(forms.Form):
     name=forms.CharField()
+    fatrer_name=forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
+    agree=forms.BooleanField()
+    datefild=forms.DateField(widget=forms.NumberInput(attrs={'type':'date'}))
+    
+    BIRTH_YEAR_CHOICES = ['230', '1992', '2003']
+    birth_day=forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+
